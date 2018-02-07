f5e33ec65076611b11e59857a4c770005141255f
Orest Kostiuk <xxmolekula@gmail.com>
2018-02-07 21:05:50 +0200

    add gem Shoulda Matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,7 +29,19 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
 
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      # Choose a test framework:
+      with.test_framework :rspec
 
+      # Choose one or more libraries:
+      with.library :active_record
+      with.library :active_model
+      with.library :action_controller
+      # Or, choose the following (which implies all of the above):
+      with.library :rails
+    end
+  end
   config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
