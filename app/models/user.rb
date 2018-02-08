5eef904f07c888130aeaefdf17ad0edb6abcc0b3
Orest Kostiuk <xxmolekula@gmail.com>
2018-02-08 21:07:30 +0200

    add header

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  before_validation :set_admin
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def set_admin
+    self.admin = true if User.all == []
+  end
 end
